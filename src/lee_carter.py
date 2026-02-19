8f3bb3b59c3916466ccb3c929db9c0bd312a0176
jakharaz <akharazjihane@gmail.com>
2026-02-19 01:35:56 +0000

    correct downward kt trend

--- src/lee_carter.py
+++ src/lee_carter.py
@@ -26,31 +26,23 @@ def prepare_mx_matrix(df, sex="Female", age_min=0, age_max=90):
     return log_mx
 
 def fit_lee_carter(log_mx):
-    """
-    Fit Lee-Carter model using SVD.
 
-    log(m_x,t) = a_x + b_x k_t
-    """
-
-    # 1. Compute a_x
     ax = log_mx.mean(axis=1)
-
-    # 2. Center matrix
     M_centered = log_mx.sub(ax, axis=0)
 
-    # 3. SVD
     U, S, Vt = np.linalg.svd(M_centered, full_matrices=False)
 
-    # 4. Extract first component
     bx = U[:, 0]
     kt = S[0] * Vt[0, :]
 
-    # 5. Identification constraints
+    # Identification
     bx = bx / bx.sum()
     kt = kt * bx.sum()
-
     kt = kt - kt.mean()
 
-    
+    # Demographic sign convention
+    if kt[0] < kt[-1]:
+        bx = -bx
+        kt = -kt
 
     return ax, bx, kt
